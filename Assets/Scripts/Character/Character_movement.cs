6ce5aeed425b0acb2a7ba8f651a3fcd2fa466aad
unknown <you@example.com>
2024-09-12 21:00:59 +0300

    first commit4

--- Assets/Scripts/Character/Character_movement.cs
+++ Assets/Scripts/Character/Character_movement.cs
@@ -10,12 +10,19 @@ public class Character_movement : MonoBehaviour
     Vector2 moveDirection;
     float moveX;
     float moveY;
+
+    Animator animator;
+    float moveXstand;
+    float moveYstand;
+
+
     
     
     // Start is called before the first frame update
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -25,11 +32,44 @@ public class Character_movement : MonoBehaviour
         moveY = Input.GetAxisRaw("Vertical");
 
         moveDirection = new Vector2(moveX, moveY).normalized;
-        
+        if (moveX!=0 || moveY != 0)
+        {
+            Animate(moveX, moveY,1);
+            moveXstand = moveX; 
+            moveYstand = moveY;
+            Debug.Log(moveXstand+" "+ moveYstand);
+       
+        }
+        else
+        {
+            Animate(moveXstand, moveYstand, 0);
+
+        }
+
     }
 
     private void FixedUpdate()
     {
         characterController.Move(moveDirection * moveSpeed * Time.deltaTime);
+        
+    }
+    void Animate(float moveX,float moveY,int layer)
+    {
+      
+        animator.SetFloat("MoveX", moveX);
+        animator.SetFloat("MoveY", moveY);
+        switch (layer)
+        {
+            case 0:animator.SetLayerWeight(0, 1);
+                animator.SetLayerWeight(1, 0);
+                break;
+            case 1:
+                animator.SetLayerWeight(1, 1);
+                animator.SetLayerWeight(0, 1);
+                break;
+
+        }
+        animator.SetFloat("MoveX", moveX);
+        animator.SetFloat("MoveY", moveY);
     }
 }
